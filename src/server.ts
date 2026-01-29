c45b10c77ce95f1358a8d143f1b1bbca970ce267
Bruno Girome <brunogirome@gmail.com>
2026-01-29 15:06:47 -0300

    Installed knex and configured better-sqlite3

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,11 @@
 import fastify from 'fastify';
+import { knex } from './database.js';
 
 const app = fastify();
 
-app.listen({ port: 3333 }).then(() => console.log('Server Running!'));
+app.get('/test', async () => {
+  const test = await knex('sqlite_schema').select('*');
+  return test;
+});
+
+app.listen({ port: 3333 }).then(() => console.log('Server Running! 💀'));
