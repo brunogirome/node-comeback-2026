081f10396c8af8e66899451932ca371ed63556a9
Bruno Girome <brunogirome@gmail.com>
2026-01-30 19:01:04 -0300

    dotenv file

--- knexfile.ts
+++ knexfile.ts
@@ -1,9 +1,14 @@
+import 'dotenv/config';
 import type { Knex } from 'knex';
+
+if (!process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL is not set in environment variables');
+}
 
 const config: Knex.Config = {
   client: 'better-sqlite3',
   connection: {
-    filename: './db/app.db',
+    filename: process.env.DATABASE_URL,
   },
   useNullAsDefault: true,
   migrations: {
