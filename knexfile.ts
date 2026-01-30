9f4029ed7f10c6539b22cb9ac795eb2a5ff24290
Bruno Girome <brunogirome@gmail.com>
2026-01-30 19:11:28 -0300

    treat erros and zod integration

--- knexfile.ts
+++ knexfile.ts
@@ -1,14 +1,10 @@
-import 'dotenv/config';
 import type { Knex } from 'knex';
-
-if (!process.env.DATABASE_URL) {
-  throw new Error('DATABASE_URL is not set in environment variables');
-}
+import { env } from './src/env/index.js';
 
 const config: Knex.Config = {
   client: 'better-sqlite3',
   connection: {
-    filename: process.env.DATABASE_URL,
+    filename: env.DATABASE_URL,
   },
   useNullAsDefault: true,
   migrations: {
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import fastify from 'fastify';
 import crypto from 'node:crypto';
 import { knex } from './database.js';
+import { env } from './env/index.js';
 
 const app = fastify();
 
@@ -16,4 +17,4 @@ app.get('/test', async () => {
   return transaction;
 });
 
-app.listen({ port: 3333 }).then(() => console.log('Server Running! 💀'));
+app.listen({ port: env.PORT }).then(() => console.log('Server Running! 💀'));
