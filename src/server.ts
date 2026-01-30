df2764aecfb505ebaa7e1de9adcbc84773f009ea
Bruno Girome <brunogirome@gmail.com>
2026-01-30 15:22:47 -0300

    test insert with knex

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,19 @@
 import fastify from 'fastify';
+import crypto from 'node:crypto';
 import { knex } from './database.js';
 
 const app = fastify();
 
 app.get('/test', async () => {
-  const test = await knex('sqlite_schema').select('*');
-  return test;
+  const transaction = await knex('transactions')
+    .insert({
+      id: crypto.randomUUID(),
+      title: 'Test Transaction',
+      amount: 1000,
+    })
+    .returning('*');
+
+  return transaction;
 });
 
 app.listen({ port: 3333 }).then(() => console.log('Server Running! 💀'));
