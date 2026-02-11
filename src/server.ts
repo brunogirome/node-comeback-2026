736662473bed5d3d1a262fcf46ab5ef9c0246089
Bruno Girome <brunogirome@gmail.com>
2026-02-11 18:01:09 -0300

    Global Hook sample

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,11 @@ import { transactionsRoutes } from './routes/transactions.js';
 
 const app = fastify();
 
+// Global hook sample
+app.addHook('preHandler', async request => {
+  console.log(`[${request.method}] ${request.url}`);
+});
+
 app.register(cookie);
 app.register(transactionsRoutes, { prefix: 'transactions' });
 
