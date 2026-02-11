ea5c0e1e32f4b0ecb7977b91e8b59015780719e7
Bruno Girome <brunogirome@gmail.com>
2026-02-11 17:07:17 -0300

    Cookies for fastify

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
+import cookie from '@fastify/cookie';
 import fastify from 'fastify';
+
 import { env } from './env/index.js';
 import { transactionsRoutes } from './routes/transactions.js';
 
 const app = fastify();
 
+app.register(cookie);
 app.register(transactionsRoutes, { prefix: 'transactions' });
 
 app.listen({ port: env.PORT }).then(() => console.log('Server Running! 💀'));
