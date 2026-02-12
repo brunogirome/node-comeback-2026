d3fda2fcdae6d88e0ea40e7bddfbd4c66f8933f0
Bruno Girome <brunogirome@gmail.com>
2026-02-12 15:48:30 -0300

    more renaming

---
+++ test/transactions.spec.ts
@@ -0,0 +1,21 @@
+import request from 'supertest';
+import { afterAll, beforeAll, describe, it } from 'vitest';
+
+import { app } from '../src/app.js';
+
+describe('Transactions routes', () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('should be able to create a new transaction', async () => {
+    await request(app.server)
+      .post('/transactions')
+      .send({ title: 'Test Transaction', amount: 113, type: 'credit' })
+      .expect(201);
+  });
+});
