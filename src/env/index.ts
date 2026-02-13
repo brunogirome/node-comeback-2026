e2976848ff07a081fe1a67042356ac0b02bb8b6e
Bruno Girome <brunogirome@gmail.com>
2026-02-13 03:44:18 -0300

    Host env for render

--- src/env/index.ts
+++ src/env/index.ts
@@ -11,6 +11,7 @@ const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'production', 'test']).default('production'),
   DATABASE_CLIENT: z.enum(['better-sqlite3', 'pg']),
   DATABASE_URL: z.string().nonempty('DATABASE_URL is required'),
+  HOST: z.string().default('localhost'),
   PORT: z.coerce.number().default(3333),
 });
 
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,8 @@
 import { app } from './app.js';
 import { env } from './env/index.js';
 
-app.listen({ port: env.PORT }).then(() => console.log('Server Running! 💀'));
+app
+  .listen({ host: env.HOST, port: env.PORT })
+  .then(() =>
+    console.log(`Server Running! 💀 Address: http://${env.HOST}:${env.PORT}`)
+  );
